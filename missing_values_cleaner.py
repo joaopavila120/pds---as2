1a7df93c3e20e71ffe6b939c1d1161fc91192105
leonorribeiro04 <leonor.ribeiro@aiesec.net>
2025-10-19 12:39:14 +0100

    Funções até à 9

--- missing_values_cleaner.py
+++ missing_values_cleaner.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 def missing_values_cleaner(df) -> pd.DataFrame:
     """ Removes rows with null values only in 'CustomerID' or 'Description' columns"""
-    
+
     df = df.copy()
-    
+
     # Drop rows where 'CustomerID' OR 'Description' has a missing value
     df = df.dropna(subset=["CustomerID", "Description"])
-    
+
     return df
---
+++ top_five_cust.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def top_five_cust(df) -> list:
+
+    order_counts = df.groupby('CustomerID')['InvoiceNo'].nunique()
+
+    top_customers = order_counts.sort_values(ascending=False).head(5)
+
+    return top_customers.index.tolist()
