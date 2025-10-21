d418678188549262fff9ca5da89b94c3c21bfb5c
joaopaulo120 <joaopaulo1448@gmail.com>
2025-10-21 10:47:30 +0100

    ultimas funcoes

--- top_five_spenders.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def top_five_spenders(df) -> list:
-    total_spent = df.groupby('CustomerID')['amount_spent'].sum()
-
-    top_spenders = total_spent.sort_values(ascending=False).head(5)
-
-    return top_spenders.index.tolist()
